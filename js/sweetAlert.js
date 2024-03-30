5cee8d2806780ca81f164f04c4f98d6e59812ca8
Charlyt0 <114592843+Charlyt0@users.noreply.github.com>
2024-03-29 22:24:50 -0300

    Add files via upload

---
+++ js/sweetAlert.js
@@ -0,0 +1,7 @@
+Swal.fire({
+title: "¡¡¡Preparad@s para la boda del año!!!",
+confirmButtonText: "Si, estoy preparad@",
+confirmButtonColor: "#ffbba8",
+imageUrl: "img/amigoss.jpg",
+imageWidth: "200px",
+});
