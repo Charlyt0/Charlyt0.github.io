bec51e07f6b3b6bb5bf0b34736f6705827a93149
Charlyt0 <114592843+Charlyt0@users.noreply.github.com>
2024-03-30 21:18:39 -0300

    Update sweetAlert.js

--- js/sweetAlert.js
+++ js/sweetAlert.js
@@ -3,7 +3,7 @@ title: "¡¡¡Preparad@s para la boda del año!!!",
 confirmButtonText: "Si, estoy preparad@",
 confirmButtonColor: "#ffbba8",
 imageUrl: "img/amigoss.jpg",
-imageWidth: "200px",
+imageWidth: "150px",
 widht: "100%",
 position: "center",
 });
