0462a032fa62166b67f68a925c64bf1f57c984cc
Charlyt0 <114592843+Charlyt0@users.noreply.github.com>
2024-03-30 23:19:34 -0300

    Add files via upload

--- js/sweetAlert.js
+++ js/sweetAlert.js
@@ -3,7 +3,7 @@ title: "¡¡¡Preparad@s para la boda del año!!!",
 confirmButtonText: "Si, estoy preparad@",
 confirmButtonColor: "#ffbba8",
 imageUrl: "img/amigoss.jpg",
-imageWidth: "150px",
+imageWidth: "200px",
 widht: "100%",
 position: "center",
 });
