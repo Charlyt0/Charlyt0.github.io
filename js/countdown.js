57d5d4fb0e3b149cfb21a7c17d162a0f52ca8d2b
Charlyt0 <114592843+Charlyt0@users.noreply.github.com>
2024-04-03 20:25:10 -0300

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,9 +1,9 @@
 simplyCountdown('#cuenta', {
 	year: 2024, // required
-	month: 4, // required
-	day: 2, // required
-	hours: 22, // Default is 0 [0-23] integer
-	minutes: 20, // Default is 0 [0-59] integer
+	month: 10, // required
+	day: 18, // required
+	hours: 0, // Default is 0 [0-23] integer
+	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
 		days: 'Día',
